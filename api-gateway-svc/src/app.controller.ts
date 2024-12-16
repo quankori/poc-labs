d076ae6f1525f3c7c5d28a595fec2c10e42ce3ca
Quan Kori <minhquan.k0r1@gmail.com>
2024-12-16 20:43:46 +0700

    Health

--- api-gateway-svc/src/app.controller.ts
+++ api-gateway-svc/src/app.controller.ts
@@ -1,12 +1,27 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  DiskHealthIndicator,
+  HealthCheck,
+  HealthCheckService,
+  HttpHealthIndicator,
+} from '@nestjs/terminus';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private health: HealthCheckService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/heath')
+  @HealthCheck()
+  check() {
+    return this.health.check([]);
   }
 }
